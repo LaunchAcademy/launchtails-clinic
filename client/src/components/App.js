429b31ac9b2b2f6780d3d25128f70805f5cb4029
Pat <patrickawhelan@gmail.com>
2021-03-11 15:37:00 -0500

    updated starting state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,13 +8,8 @@ import DrinkShow from "./DrinkShow"
 const App = (props) => {
   return (
     <div className="grid-container">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={DrinksIndexContainer} />
-          <Route exact path="/drink/:id" component={DrinkShow} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+      Launch tails.... a woo ooh?
+   </div>
   )
 }
 
